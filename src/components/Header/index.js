7ef4d178cab7732a227f5ad73be7da131fc1a7e6
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-07-17 04:36:21 -0500

    Customizing site

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import Link from 'gatsby-link';
+import React from "react";
+import Link from "gatsby-link";
 
-import Wrapper from './Wrapper';
-import LogoWrapper from './LogoWrapper';
-import NavWrapper from './NavWrapper';
-import Logo from './logo.png';
-import Nav from '../Nav';
+import Wrapper from "./Wrapper";
+import LogoWrapper from "./LogoWrapper";
+import NavWrapper from "./NavWrapper";
+import Logo from "./logo.jpg";
+import Nav from "../Nav";
 
 function Header() {
   return (
     <Wrapper id="top">
       <LogoWrapper>
-        <Link to="/"><img src={Logo} alt="Ryan Fitzgerald"/></Link>
+        <Link to="/">
+          <img src={Logo} alt="Adamu M. Dankore" />
+        </Link>
       </LogoWrapper>
       <NavWrapper>
-        <Nav/>
-      </NavWrapper>  
+        <Nav />
+      </NavWrapper>
     </Wrapper>
   );
 }
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import Wrapper from './Wrapper';
-import Container from './Container';
-import Column from './Column';
-import H1 from './H1';
-import P from './P';
-import HeroLinks from '../HeroLinks';
-import Img from './Img';
-import Me from './super-cool-dude.png';
-import ParticlesBG from '../ParticlesBG';
+import Wrapper from "./Wrapper";
+import Container from "./Container";
+import Column from "./Column";
+import H1 from "./H1";
+import P from "./P";
+import HeroLinks from "../HeroLinks";
+import Img from "./Img";
+import Me from "./super-cool-dude.png";
+import ParticlesBG from "../ParticlesBG";
 
 function Hero(props) {
   return (
     <Wrapper>
-      <ParticlesBG/>
+      <ParticlesBG />
       <Container>
         <Column>
           <H1>Ryan Fitzgerald</H1>
           <P>
-            Full-stack Software Engineer based in Toronto, Canada. Currently, I'm a Full-Stack Developer at <a href="https://www.ambyint.com/" target="_blank">Ambyint</a>.
+            Full-stack Software Engineer. Currently, I'm a Junior Front-End
+            Developer at{" "}
+            <a href="https://homeawayfromhome.online/" target="_blank">
+              Home Away Frome Home
+            </a>
+            .
           </P>
-          <HeroLinks/>
+          <HeroLinks />
         </Column>
         <Column>
-          <Img src={Me} alt="Picture of Ryan Fitzgerald"/>
+          <Img src={Me} alt="Picture of Adamu M. Dankore" />
         </Column>
       </Container>
     </Wrapper>
