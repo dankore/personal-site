6a23e0afe56c332e09304d1e449ea0f14b3138ef
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-07-21 05:20:23 -0500

    Contact form button hover

--- src/components/BlogPrompt/button.js
+++ src/components/BlogPrompt/button.js
@@ -14,6 +14,12 @@ const Button = styled.button`
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   -webkit-transition: 0.5s ease all;
   transition: 0.5s ease all;
+
+  &:hover {
+    -webkit-box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19),
+      0 6px 6px rgba(0, 0, 0, 0.23);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  }
 `;
 
 export default Button;
