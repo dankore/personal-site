2b84bcd698d9edb1745a2802434a2d7aef4d91a0
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-07-21 00:44:42 -0500

    Remove blog nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import Link from './Link';
-import GatsbyLink from './GatsbyLink';
+import Link from "./Link";
+import GatsbyLink from "./GatsbyLink";
 
 function Nav() {
   return (
     <nav>
       <GatsbyLink to="/">Home</GatsbyLink>
       <GatsbyLink to="/#projects">Projects</GatsbyLink>
-      <Link href="https://blog.ryanfitzgerald.ca/">Blog</Link>
       <GatsbyLink to="/#contact">Contact</GatsbyLink>
     </nav>
   );
