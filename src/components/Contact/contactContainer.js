6ffb58ed2448b82702e4c3f95dfe6d858715faab
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-07-21 03:22:38 -0500

    Correct component name

--- src/components/Contact/contactContainer.js
+++ src/components/Contact/contactContainer.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-const contactWrapper = styled.div`
+const ContactContainer = styled.div`
   margin: 0 0 15px 0;
   color: #fff;
   font-weight: 500;
 `;
 
-export default contactWrapper;
+export default ContactContainer;
