bc9f5fd9bbfadd79c74c49417dc02391b42330f3
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-07-21 03:20:12 -0500

    Fix issue?

--- src/components/Contact/h2.js
+++ src/components/Contact/h2.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-const formH2 = styled.h2`
+const H2 = styled.h2`
   margin: 0 0 15px 0;
   color: #fff;
   font-weight: 500;
 `;
 
-export default formH2;
+export default H2;
