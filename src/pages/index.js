2ab7579d159d8251dd657f120731331f877b77aa
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-08-31 23:13:17 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -155,6 +155,15 @@ class BlogIndex extends React.Component {
            <Container>
               <SectionHeader>About Me</SectionHeader>
               <AboutMeParagraph>
+                I'm a software engineer who specialises in responsive web designs. Since November 2018, I have been 
+                working part-time for my internet startup, HAFH, as a front-end developer, where I help with designs, 
+                static pages, SEO, and marketing. 
+                
+                As the head of the marketing team, we helped grew the startup to more than 740* registered users, 4,000* 
+                unique visits, and more than 500,000* SSL requests served since our beta in launch on May 22, 2019.
+
+                I'm also a self-taught developer.
+                <em>*As of August, 2019</em>
                 
               </AboutMeParagraph>
            </Container>
