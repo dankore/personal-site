4751f39ee29617d8ef23e3c705cde55ef5b8db1e
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-09-21 02:42:43 -0500

    Update index.js

--- src/components/HeroLinks/index.js
+++ src/components/HeroLinks/index.js
@@ -7,7 +7,7 @@ import SocialMedia from '../SocialMedia';
 function HeroLinks(props) {
   return (
     <div>
-      <Button href="https://1drv.ms/b/s!AuOdQ10mJ7QNhM8FglOUqFLgbBhlqA?e=T1O0ZT">View Resume</Button>
+      <Button href="https://onedrive.live.com/view.aspx?cid=0DB427265D439DE3&authKey=%21AIJTlKhS4GwYZag&resid=DB427265D439DE3%2175653&ithint=%2Epdf&open=true&app=WordPdf">View Resume</Button>
       <SocialWrapper>
         <SocialMedia/>
       </SocialWrapper>
