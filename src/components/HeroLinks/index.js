7f534fee7ca66b3be0fbf688d2b9cc0df0750a5a
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-09-21 02:03:04 -0500

    Update index.js

--- src/components/HeroLinks/index.js
+++ src/components/HeroLinks/index.js
@@ -7,7 +7,7 @@ import SocialMedia from '../SocialMedia';
 function HeroLinks(props) {
   return (
     <div>
-      <Button href="https://documentcloud.adobe.com/link/track?uri=urn%3Aaaid%3Ascds%3AUS%3A45480876-c88b-4266-9df1-96ba0f963944">View Resume</Button>
+      <Button href="https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:bd3cc402-1ddc-4f78-90de-f24a20e6d7c2">View Resume</Button>
       <SocialWrapper>
         <SocialMedia/>
       </SocialWrapper>
