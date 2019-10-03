ff968e481318dbaf25d696067ad398071e843184
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-10-03 08:54:21 -0500

    Update index.js

--- src/components/HeroLinks/index.js
+++ src/components/HeroLinks/index.js
@@ -7,7 +7,7 @@ import SocialMedia from '../SocialMedia';
 function HeroLinks(props) {
   return (
     <div>
-      <Button href="https://rawcdn.githack.com/dankore/resume/0be9c76da33deb0c8f0698aeb9ae1376ec501dec/src/Adamu_M_Dankore_Resume.pdf">View Resume</Button>
+      <Button href="https://cdn.statically.io/gh/dankore/resume/00f5bc1a/Adamu_M_Dankore_Resume.pdf">View Resume</Button>
       <SocialWrapper>
         <SocialMedia/>
       </SocialWrapper>
